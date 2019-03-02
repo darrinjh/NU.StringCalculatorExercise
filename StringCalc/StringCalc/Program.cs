4740468a232717eeff5b190d4885f015ad6229f7
darrinjh <darrin@bijousoftware.com>
2019-03-02 15:06:14 +0000

    Step4

--- StringCalc/StringCalc/Program.cs
+++ StringCalc/StringCalc/Program.cs
@@ -22,6 +22,47 @@ namespace StringCalc
         {
             int retval = 0;
             if (numbers.Contains(",\\n")) return retval; // not sure if you wanted a full carrage return so I went with what it said.
+
+            string[] numbersList = findNumbers(numbers);
+            if (numbers.Length > 0)
+            {
+                foreach (string x in numbersList)
+                {
+                    if (x != "")
+                    {
+                        try
+                        {
+                            retval += Int32.Parse(x);
+                        } catch (Exception ex)
+                        {
+                        }                       
+                    }
+                }
+            }
+            return retval;
+        }
+
+        static string[] findNumbers(string t)
+        {
+            char delim =',';
+            string[] retval1 = Regex.Split(t, @"\D+"); ;
+
+            if (t.Length > 5)
+            {
+                if (t.Substring(0, 2) == "//" && t.Substring(3, 2) == "\\n")
+                {
+                    delim = t.Substring(2, 1)[0];
+                    retval1 = t.Substring(5).Split(delim);
+                }
+            }
+            return retval1;
+        }
+
+        //Step 3
+        static int Step3Add(string numbers)
+        {
+            int retval = 0;
+            if (numbers.Contains(",\\n")) return retval; // not sure if you wanted a full carrage return so I went with what it said.
           
             string[] numbersList = Regex.Split(numbers, @"\D+");
             if (numbers.Length > 0)
